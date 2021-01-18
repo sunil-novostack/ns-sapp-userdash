a602249a89fb5575d5fbe34fa5b37ddcfeead480
sunil kumar mutaka <sunil.kumar@novostack.com>
2021-01-18 11:10:12 +0530

    heroku post setup

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+//const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
